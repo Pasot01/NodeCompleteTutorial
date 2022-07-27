3cf66b31eec767770f992ed7494e5e748ddf4461
Pasot01 <contactmyphone14@gmail.com>
2022-07-27 22:01:13 +0200

    Configuration de gitignore ainsi que package.json pour les dépendances dev et product

--- 03TUT/index.js
+++ 03TUT/index.js
@@ -1,1 +1,8 @@
-console.log('Test nodemon');
+const { format } = require('date-fns');
+const { v4: uuid } = require('uuid');
+
+console.log(format(new Date(), 'yyyy-MM-dd\tHH:mm:ss'));
+
+console.log(uuid());
+
+console.log();
